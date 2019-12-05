3e60b4d27636bf67971fe1793fea0be42aff9c04
George091 <Georgebernardbarker@gmail.com>
2019-12-05 17:51:41 -0500

    Update CNNCIFAR.py

--- CNNCIFAR.py
+++ CNNCIFAR.py
@@ -13,11 +13,12 @@ Created on Thu Dec  5 14:05:28 2019
 """
 
 import keras
+import pickle
 from keras.datasets import cifar10
-from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
+from keras import regularizers
 
 num_classes = 10
 
@@ -35,20 +36,25 @@ x_test /= 255
 y_train = keras.utils.to_categorical(y_train)
 y_test = keras.utils.to_categorical(y_test)
 
+weight_decay = 1e-5
+
 model = Sequential()
-model.add(Conv2D(32, (3, 3), activation='relu', padding='valid', input_shape=x_train.shape[1:]))
+model.add(Conv2D(32, (3, 3), activation='relu', padding='valid', kernel_regularizer=regularizers.l2(weight_decay), input_shape=x_train.shape[1:]))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
 
-model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(Conv2D(32, (3, 3), activation='relu', kernel_regularizer=regularizers.l2(weight_decay)))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
 
-model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu', kernel_regularizer=regularizers.l2(weight_decay)))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
 
 model.add(Flatten())
 model.add(Dense(500))
 model.add(Activation('relu'))
-model.add(Dropout(0.5))
+model.add(Dropout(0.4))
 model.add(Dense(num_classes))
 model.add(Activation('softmax'))
 
@@ -57,4 +63,13 @@ model.compile(loss='categorical_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=4, batch_size=64, verbose=1)
+model.fit(x_train, y_train, epochs=50, batch_size=64, verbose=1)
+
+# Evaluate model
+print("Evaluating Test Set: ")
+predictions = model.evaluate(x_test, y_test)
+print("accuracy: %.2f%%" % (predictions[1]*100))
+
+pickle_out = open("CNN-CIFAR","wb")
+pickle.dump(model, pickle_out)
+pickle_out.close()
